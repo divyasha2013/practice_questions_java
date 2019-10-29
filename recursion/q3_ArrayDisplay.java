209c527ed4b5f3e2226d904fad2dc6a80de2850f
divyasha2013 <55696537+divyasha2013@users.noreply.github.com>
2019-10-29 15:40:52 +0530

    Create q3_ArrayDisplay.java

---
+++ recursion/q3_ArrayDisplay.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class q3_ArrayDisplay
+{
+    public static void display1(int[] arr,int indx)
+    {
+        if(indx==arr.length)
+           return;
+        
+        System.out.println(arr[indx]);
+        display1(arr,indx+1);
+
+    }
+    
+    public static void display2(int[] arr,int indx)
+    {
+        if(indx==arr.length)
+        return;
+        
+        display2(arr,indx+1);
+        System.out.println(arr[indx]);
+
+    }
+    
+    public static void main(String[] args)
+    {
+        int[] arr={10,20,30,40,50};
+        display1(arr,0);
+        display2(arr,0);
+    }
+}
